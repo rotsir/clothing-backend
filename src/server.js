63b283b42f20a90d8cd95ca7f70b8a736a86238c
rotsir <148975309+rotsir@users.noreply.github.com>
2026-04-17 22:01:25 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -36,3 +36,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: "Test route working" });
+});
